925664087992be88726fcc36f943723cd1f9c99f
Veevek Dave <91489412+veevekdave@users.noreply.github.com>
2022-10-11 14:47:41 -0400

    Fixed marker color on google map

--- Dining Hall Page/script.js
+++ Dining Hall Page/script.js
@@ -7,24 +7,10 @@ function initMap(){
   //new map
   var map = new google.maps.Map(document.getElementById('map'), options);
 
-  addMarker(map, {lat: 42.7284 , lng: -73.6745}, "<h4>Commons</h4>");
-  addMarker(map, {lat: 42.7297 , lng: -73.6781}, "<h4>Sage</h4>");
-  addMarker(map, {lat: 42.73116 , lng: -73.68577}, "<h4>Blitman</h4>");
-  addMarker(map, {lat: 42.73105 , lng: -73.67124}, "<h4>BarH</h4>");
-  // //markers
-  // var marker = new google.maps.Marker({
-  //   position: {lat: 42.7284 , lng: -73.6745},
-  //   map: map
-  // });
-
-  // var infoWindow = new google.maps.InfoWindow({
-  //   content:'<p>Commmons Dining Hall</p>'
-  // })
-
-  // marker.addListener('click', function(){
-  //   infoWindow.open(map, marker);
-  // })
-
+  addMarker(map, {lat: 42.7284 , lng: -73.6745}, "<h4 style='color: black'>Commons</h4>");
+  addMarker(map, {lat: 42.7297 , lng: -73.6781}, "<h4 style='color: black'>Sage</h4>");
+  addMarker(map, {lat: 42.73116 , lng: -73.68577}, "<h4 style='color: black'>Blitman</h4>");
+  addMarker(map, {lat: 42.73105 , lng: -73.67124}, "<h4 style='color: black'>BarH</h4>");
 
 }
 
