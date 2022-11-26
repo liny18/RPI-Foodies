2a619e493e1ec2a89c081bcad3c03c442733aee5
hicker2 <91489334+hicker2@users.noreply.github.com>
2022-11-26 15:37:23 -0500

    Added dropdown options to the uploads page for tags and location
    I added dropdown options to the uploads page for locations and tags this way they will be stored along with the other info. I also changed all the files to php files and used the same headers and footers for each one. ONE MASSIVE BUG IS ON THE MAIN PAGE I HAVE NO IDEA WHY THE FOOTER IS NOT AT THE BOTTOM

--- project/phpcas/index.php
+++ project/phpcas/index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../pictures/RPIFoodies.png">
     <title>RPI Foodies</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="./login.css">
     <script src="./login.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,40 +17,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../style.css">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-md sticky-top navbar-light header">
-        <a class="navbar-brand refresh" href="../main/main.html">
-        <img src="../pictures/RPIFoodies.png" alt="Error Cats logo" width="40" height="40" style="border-radius: 50%;">
-        RPI Foodies
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline justify-content-center align-items-center container">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-light" type="submit">
-                <img src="../pictures/search_ideogram.svg" alt="Magnifying glass" width="30" height="30">
-            </button>
-        </form>
-        <ul class="navbar-nav ml-auto align-items-center">
-            <li class="nav-item trending">
-                <a href="#" class="navbar-brand">
-                    <img src="../pictures/trendingIcon.svg" alt="trending button" width="40" height="40">
-                </a>
-            </li>
-            <li class="nav-item post">
-                <a href="../uploadPage/upload.html" class="navbar-brand">
-                    <img src="../pictures/addPostIcon.svg" alt="add post button" width="40" height="40">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="../project-login/create.html" class="btn btn-dark" role="button">Sign Up</a>
-            </li>
-        </ul>
-        </div>
-    </nav>
+    <header>
+        <?php include '../header.html'; ?>
+    </header>
 
     <main>
         <h1 class="main-slogan"> RPI Foodies, find out what you love!</h1>
@@ -60,7 +33,7 @@
                 <div class="form-container">
                     <?php
                     include_once("./CAS-1.4.0/CAS.php");
-                    phpCAS::client(CAS_VERSION_2_0,'cas.auth.rpi.edu',443,'/cas');
+                    phpCAS::client(CAS_VERSION_2_0, 'cas.auth.rpi.edu', 443, '/cas');
 
                     // This is not recommended in the real world!
                     // But we don't have the apparatus to install our own certs...
@@ -73,26 +46,20 @@
                         echo "<a href='login.php' class='login_button'>Login</a>";
                     }
                     ?>
-                    <p class="cas_info">*This website is connected to RPI CAS login system, you need to have a RPI account to login.</p>
+                    <p class="cas_info">*This website is connected to RPI CAS login system, you need to have a RPI
+                        account to login.</p>
                 </div>
             </div>
         </div>
     </main>
     <footer>
-        <div class="container foot">
-          <div class="foot-container d-flex justify-content-between">
-            <div class="d-flex align-content-center">
-              <a href="#">
-                <img src="../pictures/logo.png" alt="team-logo" class="foot-logo">
-              </a>
-              <p class="pl-3" >© 2022 ErrorCats</p>
-            </div>
-            <a href="https://github.com/RPI-ITWS/ITWS2110-F22-ErrorCats" target="_blank">
-              <img class="foot-logo" src="../pictures/git.png" alt="git-link">
-            </a>
-          </div>
-        </div>
-      </footer>
+        <?php include '../footer.html'; ?>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 </body>
+
 </html>
