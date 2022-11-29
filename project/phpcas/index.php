adc36b8c88f618e6fb0a6e5e85879fb5795a0e02
ShadowSkeleton <fengj5@rpi.edu>
2022-11-29 17:46:23 -0500

    Update index.php

--- project/phpcas/index.php
+++ project/phpcas/index.php
@@ -50,9 +50,14 @@
                         // get the user's username, this is the RCS id of the user, this is the userID in the table
                         $username = phpCAS::getUser();
                         // check if the userID is already in the database, if not, insert the userID into the database, and make the username same with the userID as default
-                        // using insert into values where not exists to avoid duplicate username
-                        $sql = "INSERT INTO users (username,admin) VALUES ('$username', 0) WHERE NOT EXISTS (SELECT username FROM users WHERE username = '$username')";
-                        $db->exec($sql);
+                        // if the userID is already in the database, do nothing
+                        $sql = "SELECT * FROM users WHERE userID = '$username'";
+                        $result = $db->query($sql)->fetch();
+                        if ($result[0]==0) {
+                            $sql = "INSERT INTO users (username, admin) VALUES ('$username',0)";
+                            $db->exec($sql);
+                        }
+
                         // get the userID from the database
                         $sql = "SELECT userID FROM users WHERE username = '$username'";
                         $result = $db->query($sql);
