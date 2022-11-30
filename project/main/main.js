f3fad72c3072336e81d6a9128f1b365805f897e9
hicker2 <91489334+hicker2@users.noreply.github.com>
2022-11-30 16:40:50 -0500

    Update main.js

--- project/main/main.js
+++ project/main/main.js
@@ -1,5 +1,7 @@
 // function to run a php script using ajax which increases the like counter
 // or decreases the like counter by 1
+// https://www.w3schools.com/xml/ajax_database.asp
+// w3 got me covered ngl
 function likeCounter(postID, userID, text) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
