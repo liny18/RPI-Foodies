baf35667a6d7cadc7494bc725eccc24a7eed968d
Veevek Dave <91489412+veevekdave@users.noreply.github.com>
2022-12-06 20:34:46 -0500

    Update banned.php

--- project/customize_error_page/banned.php
+++ project/customize_error_page/banned.php
@@ -9,7 +9,7 @@
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css" rel="stylesheet">
     <link rel="stylesheet" href="../style.css">
     <link rel="icon" type="image/x-icon" href="../pictures/RPIFoodies.png">
-    <title>404 Not Found</title>
+    <title>You're banned</title>
     <link rel="stylesheet" href="./error_style.css">
     <script defer src="https://kit.fontawesome.com/bb67f860a0.js" crossorigin="anonymous"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
